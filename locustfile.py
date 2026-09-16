caffaa16b9c1cfe59d32efd06755b2676f715357
OlegGayvoronsky <o.gajvoronskij03@gmail.com>
2026-09-16 09:39:11 +0300

    have added locustfile

---
+++ locustfile.py
@@ -0,0 +1,30 @@
+from locust import HttpUser, task, between
+
+
+class ChurnUser(HttpUser):
+    wait_time = between(0.5, 1.5)
+
+    good_row = {
+        "Surname": "Smith",
+        "CreditScore": 667,
+        "Geography": "France",
+        "Gender": "Female",
+        "Age": 51,
+        "Tenure": 1,
+        "Balance": 0.0,
+        "NumOfProducts": 1,
+        "HasCrCard": False,
+        "IsActiveMember": False,
+        "EstimatedSalary": 129870.93,
+    }
+
+    @task(5)
+    def predict(self):
+        self.client.post(
+            "/v1/predict",
+            json=self.good_row,
+        )
+
+    @task(1)
+    def health(self):
+        self.client.get("/health")
